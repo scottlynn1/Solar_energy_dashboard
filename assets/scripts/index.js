53c41dd5b42d93c351a2808887c394151d301ae2
scottlynn1 <scottlynn234@yahoo.com>
2025-02-05 12:16:38 -0500

    add ability to change form data, resubmit and update chart based on new data

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,75 +1,71 @@
 import Chart from 'chart.js/auto'
 
-setTimeout(function() {
-  const form = document.getElementById('form');
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const formdata = new FormData(form);
-    const params = new URLSearchParams({
-      "system_capacity": formdata.get('system_capacity'),
-      "module_type": formdata.get('module_type'),
-      "losses": formdata.get('losses'),
-      "array_type": formdata.get('array_type'),
-      "tilt": formdata.get('tilt'),
-      "azimuth": formdata.get('azimuth'),
-      "lat": formdata.get('lat'),
-      "lon": formdata.get('lon')
-    });
-    fetch(`solarapi?${params}`, {
-      method: "GET",
-      headers: {
-        "accept": "application/json",
-        "X-Requested-With": "XMLHttpRequest",
-      }
-    }).then(response => response.json()).then(returndata => {
-      const solardata = [
-        { month: 'jan', kWh: returndata.outputs.dc_monthly[0] },
-        { month: 'feb', kWh: returndata.outputs.dc_monthly[1] },
-        { month: 'mar', kWh: returndata.outputs.dc_monthly[2] },
-        { month: 'apr', kWh: returndata.outputs.dc_monthly[3] },
-        { month: 'may', kWh: returndata.outputs.dc_monthly[4] },
-        { month: 'jun', kWh: returndata.outputs.dc_monthly[5] },
-        { month: 'jul', kWh: returndata.outputs.dc_monthly[6] },
-        { month: 'aug', kWh: returndata.outputs.dc_monthly[7] },
-        { month: 'sep', kWh: returndata.outputs.dc_monthly[8] },
-        { month: 'oct', kWh: returndata.outputs.dc_monthly[9] },
-        { month: 'nov', kWh: returndata.outputs.dc_monthly[10] },
-        { month: 'dec', kWh: returndata.outputs.dc_monthly[11] },
-      ];
-      new Chart(
-        document.getElementById('acquisitions'),
-        {
-          type: 'bar',
-          data: {
-            labels: solardata.map(row => row.month),
-            datasets: [
-              {
-                label: 'Acquisitions by year',
-                data: solardata.map(row => row.kWh)
-              }
-            ]
-          }
-        }
-      );
-    });
-  });
-}, 5000);
 
-/*
-setTimeout(function() {
+const form = document.getElementById('form');
+const barchart = new Chart(document.getElementById('acquisitions'), {type: "bar", data: {labels: [], datasets: [{label: "DC Production by Month", data: []}]}})
+console.log(barchart.data.labels);
+
+function addData(chart, data) {
+  data.forEach(row => {
+    chart.data.labels.push(row.month);
+    chart.data.datasets[0].data.push(row.kWh);
+  });
+  chart.update();
+}
+
+function clearChart(chart) {
+  if (chart.data.labels.length !==0) {
+    chart.data.labels.length = 0;
+    chart.data.datasets[0].data.length = 0;
+    chart.update();
+  } else {
+    console.log('no data to clear');
+  }
+
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const formdata = new FormData(form);
+  const params = new URLSearchParams({
+    "system_capacity": formdata.get('system_capacity'),
+    "module_type": formdata.get('module_type'),
+    "losses": formdata.get('losses'),
+    "array_type": formdata.get('array_type'),
+    "tilt": formdata.get('tilt'),
+    "azimuth": formdata.get('azimuth'),
+    "lat": formdata.get('lat'),
+    "lon": formdata.get('lon')
+  });
   fetch(`solarapi?${params}`, {
     method: "GET",
     headers: {
       "accept": "application/json",
       "X-Requested-With": "XMLHttpRequest",
     }
-  }).then(response => response.json()).then(response => 
-    document.getElementById('data').innerText = JSON.stringify(response)
-  );
-}, 5000);
+  }).then(response => response.json()).then(returndata => {
+    const solardata = [
+      { month: 'jan', kWh: returndata.outputs.dc_monthly[0] },
+      { month: 'feb', kWh: returndata.outputs.dc_monthly[1] },
+      { month: 'mar', kWh: returndata.outputs.dc_monthly[2] },
+      { month: 'apr', kWh: returndata.outputs.dc_monthly[3] },
+      { month: 'may', kWh: returndata.outputs.dc_monthly[4] },
+      { month: 'jun', kWh: returndata.outputs.dc_monthly[5] },
+      { month: 'jul', kWh: returndata.outputs.dc_monthly[6] },
+      { month: 'aug', kWh: returndata.outputs.dc_monthly[7] },
+      { month: 'sep', kWh: returndata.outputs.dc_monthly[8] },
+      { month: 'oct', kWh: returndata.outputs.dc_monthly[9] },
+      { month: 'nov', kWh: returndata.outputs.dc_monthly[10] },
+      { month: 'dec', kWh: returndata.outputs.dc_monthly[11] },
+    ];
+    clearChart(barchart);
+    addData(barchart, solardata);
+  });
+});
 
 
 
+/*
 setTimeout(function() {
 
   const data = [
