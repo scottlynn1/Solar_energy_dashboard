4b8b74fc2876d4207fff3702b70b5aa57fbbe932
scottlynn1 <scottlynn234@yahoo.com>
2025-02-17 09:41:24 -0500

    create user signup page using django class bass view template for now

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-
+from .views import signup_view
 app_name = 'users'
 urlpatterns = [
   path('', views.index, name='index'),
   path('login', views.login_view, name='login'),
   path('logout', views.logout_view, name='logout'),
+  path('signup', signup_view.as_view(), name='signup')
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,11 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
 # Create your views here.
 @login_required(login_url='/login')
 def index(request):
@@ -29,3 +32,8 @@ def logout_view(request):
   return render(request, "users/login.html", {
     "message": "Logged out."
   })
+
+class signup_view(CreateView):
+  form_class = UserCreationForm
+  success_url = reverse_lazy("users:login")
+  template_name = 'users/createaccount.html'
